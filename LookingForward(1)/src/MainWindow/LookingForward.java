68f9a821ae8f31e9f50f829cc0015dd55366559e
AlexanderPHuynh <alexanderhuynh2000@gmail.com>
2024-04-17 12:25:39 -0500

    GUI pop up fix
    I fixed ur issue Lea

--- LookingForward(1)/src/MainWindow/LookingForward.java
+++ LookingForward(1)/src/MainWindow/LookingForward.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package MainWindow;
+
+import javax.swing.JFrame;
 import Timer.CreateTimer;
 import Timer.EndTimer;
 import progressBars.ProgressBarGraphics;
@@ -10,33 +8,15 @@ import model.Quit;
 import Timer.SaveTimer;
 import Timer.TimeAndDate;
 
-/**
- *
- * @author aly35
- */
-public class LookingForward{
-
-    /**
-     * @param args the command line arguments
-     */
+public class LookingForward {
     public static void main(String[] args) {
-        // TODO code application logic here
-    MainGui mainGui = new MainGui();
-    
-// TODO code application logic here
-    TimeAndDate startTime = new TimeAndDate();
-    TimeAndDate endTime = new TimeAndDate();
-    
-    startTime.setCurrentTime();
-    endTime.SetDate(2024, 10, 21, 10, 10, 0);
-    
-    System.out.println(startTime.getCurrentTime());
-    System.out.println(endTime.getEndTime());
-    SaveTimer save = new SaveTimer();
-    save.SaveToFile(startTime, endTime);
-    startTime.SetDate(2024, 11, 23, 0, 0,0);
-    endTime.SetDate(2024, 9, 30, 0, 0, 0);
-    save.SaveToFile(startTime, endTime);
+        JFrame frame = new JFrame("Application Title"); // Set a title for your window
+        MainGui mainGui = new MainGui(); // Initialize your MainGui
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Ensure the application exits when the frame is closed
+        frame.getContentPane().add(mainGui); // Add the MainGui panel to the frame
+        frame.pack(); // Adjust the frame size to its content
+        frame.setLocationRelativeTo(null); // Center the frame on the screen
+        frame.setVisible(true); // Make the frame visible
     }
-    
 }
